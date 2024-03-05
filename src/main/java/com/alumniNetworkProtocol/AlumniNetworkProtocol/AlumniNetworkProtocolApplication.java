c08c3b85d9b05808c905d5a437a219e3ccb9e50a
AMIT“ <ssshwatishweta@gmail.com>
2024-03-05 18:42:56 +0530

    first commit

---
+++ src/main/java/com/alumniNetworkProtocol/AlumniNetworkProtocol/AlumniNetworkProtocolApplication.java
@@ -0,0 +1,14 @@
+package com.alumniNetworkProtocol.AlumniNetworkProtocol;
+
+import com.mysql.cj.x.protobuf.MysqlxDatatypes;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlumniNetworkProtocolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlumniNetworkProtocolApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/alumniNetworkProtocol/AlumniNetworkProtocol/AlumniNetworkProtocolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alumniNetworkProtocol.AlumniNetworkProtocol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlumniNetworkProtocolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
